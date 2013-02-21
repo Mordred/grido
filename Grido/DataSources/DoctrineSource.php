95d2d447772ca6a06ea60535413f83d1d88a5af2
Martin Jantošovič <jantosovic.martin@gmail.com>
2013-02-21 20:42:33 +0100

    DoctrineSource: FIX source code spacing

--- Grido/DataSources/DoctrineSource.php
+++ Grido/DataSources/DoctrineSource.php
@@ -35,22 +35,22 @@ class DoctrineSource extends \Nette\Object implements IDataSource {
 	/** @var array Map column to the query builder */
 	private $sortMapping;
 
-    /**
+	/**
 	 * If $sortMapping is not set and $filterMapping is set,
 	 * $filterMapping will be used also as $sortMapping.
 	 *
-     * @param Doctrine\ORM\QueryBuilder $qb
-     * @param array $filterMapping Maps columns to the DQL columns
-     * @param array $sortMapping Maps columns to the DQL columns
-     */
-    public function __construct(\Doctrine\ORM\QueryBuilder $qb, $filterMapping = NULL, $sortMapping = NULL) {
-        $this->qb = $qb;
+	 * @param Doctrine\ORM\QueryBuilder $qb
+	 * @param array $filterMapping Maps columns to the DQL columns
+	 * @param array $sortMapping Maps columns to the DQL columns
+	 */
+	public function __construct(\Doctrine\ORM\QueryBuilder $qb, $filterMapping = NULL, $sortMapping = NULL) {
+		$this->qb = $qb;
 		$this->filterMapping = $filterMapping;
 		$this->sortMapping = $sortMapping;
 
 		if (!$this->sortMapping && $this->filterMapping)
 			$this->sortMapping = $this->filterMapping;
-    }
+	}
 
 	/**
 	 * @return Doctrine\ORM\Query
@@ -59,22 +59,22 @@ class DoctrineSource extends \Nette\Object implements IDataSource {
 		return $this->qb->getQuery();
 	}
 
-    /**
-     * @return int
-     */
-    public function getCount() {
+	/**
+	 * @return int
+	 */
+	public function getCount() {
 		$paginator = new Paginator($this->getQuery());
 		return $paginator->count();
-    }
+	}
 
-    /**
+	/**
 	 * It is possible to use query builder with additional columns.
 	 * In this case, only item at index [0] is returned, because
 	 * it should be an entity object.
 	 *
-     * @return array
-     */
-    public function getData() {
+	 * @return array
+	 */
+	public function getData() {
 		// Paginator is better if the query uses ManyToMany associations
 		$usePaginator = $this->qb->getMaxResults() !== NULL || $this->qb->getFirstResult() !== NULL;
 		$data = [];
@@ -90,69 +90,69 @@ class DoctrineSource extends \Nette\Object implements IDataSource {
 				$data[] = is_array($result) ? $result[0] : $result;
 		}
 		return $data;
-    }
+	}
 
-    /**********************************************************************************************/
+	/**********************************************************************************************/
 
-    /**
+	/**
 	 * Set filter
 	 *
-     * @param array $condition
-     */
-    public function filter(array $condition) {
+	 * @param array $condition
+	 */
+	public function filter(array $condition) {
 		$condition = $this->formatFilterCondition($condition);
 		$this->qb->andWhere($condition[0]);
 		if (isset($condition[1]) && isset($condition[2]))
 			$this->qb->setParameter($condition[2], $condition[1]);
-    }
+	}
 
-    /**
+	/**
 	 * Set sorting
 	 *
-     * @param array $sorting
-     */
-    public function sort(array $sorting) {
+	 * @param array $sorting
+	 */
+	public function sort(array $sorting) {
 		foreach ($sorting as $key => $value) {
 			$column = isset($this->sortMapping[$key])
 				? $this->sortMapping[$key]
 				: $this->qb->getRootAlias() . '.' . $key;
 			$this->qb->addOrderBy($column, $value);
 		}
-    }
+	}
 
-    /**
+	/**
 	 * Set offset and limit
 	 *
-     * @param int $offset
-     * @param int $limit
-     */
-    public function limit($offset, $limit) {
+	 * @param int $offset
+	 * @param int $limit
+	 */
+	public function limit($offset, $limit) {
 		$this->qb->setFirstResult($offset)
 			->setMaxResults($limit);
-    }
+	}
 
-    /**
-     * @param string $column
-     * @param array $conditions
-     * @return array
-     */
-    public function suggest($column, array $conditions) {
-        $qb = clone $this->qb;
+	/**
+	 * @param string $column
+	 * @param array $conditions
+	 * @return array
+	 */
+	public function suggest($column, array $conditions) {
+		$qb = clone $this->qb;
 
-        foreach ($conditions as $condition) {
+		foreach ($conditions as $condition) {
 			$condition = $this->formatFilterCondition($condition);
 			$qb->andWhere($condition[0]);
 			if ($condition[1])
 				$qb->setParameter($condition[2], $condition[1]);
-        }
+		}
 
 		$suggestions = [];
 		foreach ($qb->getQuery()->getResult() as $row)
 			$suggestions[] = $row->$column;
-        return $suggestions;
-    }
+		return $suggestions;
+	}
 
-    private function formatFilterCondition(array $condition) {
+	private function formatFilterCondition(array $condition) {
 		$matches = Strings::matchAll($condition[0], '/\[([\w_-]+)\]*/');
 		$column = NULL;
 
@@ -163,7 +163,7 @@ class DoctrineSource extends \Nette\Object implements IDataSource {
 					? $this->filterMapping[$column]
 					: $this->qb->getRootAlias() . '.' . $column;
 				$condition[0] = Strings::replace($condition[0], '/' . preg_quote($match[0], '/') . '/', $mapping);
-		        $condition[0] = trim(str_replace([ '%s', '%i', '%f' ], ':' . $column, $condition[0]));
+				$condition[0] = trim(str_replace([ '%s', '%i', '%f' ], ':' . $column, $condition[0]));
 			}
 		}
 
@@ -172,7 +172,7 @@ class DoctrineSource extends \Nette\Object implements IDataSource {
 			$condition[0] = trim(str_replace([ '%s', '%i', '%f' ], '?' . $column, $condition[0]));
 		}
 
-        return [ $condition[0], isset($condition[1]) ? $condition[1] : NULL, $column ];
-    }
+		return [ $condition[0], isset($condition[1]) ? $condition[1] : NULL, $column ];
+	}
 
 }
