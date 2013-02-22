61a8daa497aba13f65996d6fe34038ca4d7992e2
Martin Jantošovič <jantosovic.martin@gmail.com>
2013-02-22 15:42:18 +0100

    Filters\Date: Added posibility to setup date format for users. User format will be automatically converted to the database format.

--- Grido/Components/Filters/Date.php
+++ Grido/Components/Filters/Date.php
@@ -26,6 +26,21 @@ class Date extends Text
     /** @var string for ->where('<column> LIKE %s', '%'.<value>.'%') */
     protected $formatValue = '%%value%';
 
+    /** @var string JS date format */
+    protected $dateFormat = 'Y-m-d';
+
+    /**
+     * @param \Grido\Grid $grid
+     * @param string $name
+     * @param string $label
+     * @param array $items for select
+     */
+    public function __construct($grid, $name, $label, $dateFormat = 'Y-m-d')
+    {
+        $this->setDateFormat($dateFormat);
+        parent::__construct($grid, $name, $label);
+    }
+
     /**
      * @return \Nette\Forms\Controls\TextInput
      */
@@ -34,6 +49,45 @@ class Date extends Text
         $control = parent::getFormControl();
         $control->controlPrototype->class[] = 'date';
         $control->controlPrototype->attrs['autocomplete'] = 'off';
+        $control->controlPrototype->attrs['data-format'] = $this->dateFormat;
         return $control;
     }
+
+    /**
+     * Set date format
+     *
+     * @param string Date format
+     * @return \Grido\Components\Filters\Date $this
+     */
+    public function setDateFormat($format)
+    {
+        $this->dateFormat = $format;
+        return $this;
+    }
+
+    /**
+     * Get date format
+     *
+     * @return string Date format
+     */
+    public function getDateFormat()
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function formatValue($value)
+    {
+        // Convert $value to the database format
+        $datetime = \DateTime::createFromFormat($this->dateFormat, $value);
+
+        if ($this->formatValue !== NULL) {
+            return str_replace(self::VALUE_IDENTIFIER, $datetime->format('Y-m-d'), $this->formatValue);
+        } else {
+            return $datetime->format('Y-m-d');
+        }
+    }
+
 }
